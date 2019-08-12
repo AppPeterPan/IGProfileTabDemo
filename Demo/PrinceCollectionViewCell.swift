9f20c6645e1ba227060169c2ab05d46fe220bcd5
peterpan <apppeterpan@gmail.com>
2019-08-12 15:24:24 +0800

    Initial Commit

---
+++ Demo/PrinceCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PrinceCollectionViewCell.swift
+//  Demo
+//
+//  Created by SHIH-YING PAN on 2019/8/4.
+//  Copyright © 2019 SHIH-YING PAN. All rights reserved.
+//
+
+import UIKit
+
+class PrinceCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
